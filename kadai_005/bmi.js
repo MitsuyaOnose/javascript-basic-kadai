93453081121621b990e8387350621101c29f35f5
MitsuyaOnose <sgx01067nif@gmail.com>
2025-10-06 19:31:45 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,10 +1,11 @@
 // 変数の宣言
 let weight;
 let height;
+let bmi;
 
 // 値の代入
 weight = 68;
 height = 1.7;
 
 // BMIの計算式
-console.log(weight / (height * height));
+console.log(bmi = weight / (height * height));
